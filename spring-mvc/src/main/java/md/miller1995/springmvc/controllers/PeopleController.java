39fbd18d18515706f668524fd4d4508d3fd5292e
miller1995 <nircaanton95@gmail.com>
2022-10-25 21:26:46 +0300

    - in PersonDAO.java create a new method save() for add a new person in database; - in Person.java add empty constructor; - in PeopleController.java add a new GetMapping method newPerson() for add a new Person, and new PostMapping method addPerson for save this person in database; - in directory people, create newPerson.html, in this file crete a label for introduce name;

--- spring-mvc/src/main/java/md/miller1995/springmvc/controllers/PeopleController.java
+++ spring-mvc/src/main/java/md/miller1995/springmvc/controllers/PeopleController.java
@@ -1,19 +1,18 @@
 package md.miller1995.springmvc.controllers;
 
 import md.miller1995.springmvc.dao.PersonDAO;
+import md.miller1995.springmvc.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
-
     private final PersonDAO personDAO;
+
 
     @Autowired
     public PeopleController(PersonDAO personDAO) {
@@ -22,19 +21,28 @@ public class PeopleController {
 
     @GetMapping()
     public String index(Model model){
-
+        // get all people from DAO
         model.addAttribute("people", personDAO.index());
-
         return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id,
                        Model model){
-
         // get 1 person before your id from DAO
         model.addAttribute("person", personDAO.show(id));
-
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person());
+        return "people/newPerson";
+    }
+
+    @PostMapping()
+    public String addPerson(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
--- spring-mvc/src/main/java/md/miller1995/springmvc/dao/PersonDAO.java
+++ spring-mvc/src/main/java/md/miller1995/springmvc/dao/PersonDAO.java
@@ -9,14 +9,15 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private List<Person> people;
+    private static int PEOPLE_ID;
 
 
     {
         people = new ArrayList<>();
 
-        people.add(new Person( 1, "Mike"));
-        people.add(new Person(2, "Bob"));
-        people.add(new Person(3, "Nick"));
+        people.add(new Person(++PEOPLE_ID, "Mike"));
+        people.add(new Person(++PEOPLE_ID, "Bob"));
+        people.add(new Person(++PEOPLE_ID, "Nick"));
 
     }
 
@@ -26,5 +27,10 @@ public class PersonDAO {
 
     public Person show(int id){
         return people.stream().filter(person -> person.getId() == id).findAny().orElse( null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_ID);
+        people.add(person);
     }
 }
