1a8e35977fe3ba9f44d58550232aa2c6e91e9d85
miller1995 <nircaanton95@gmail.com>
2022-10-26 16:46:43 +0300

    - in pom.xml add dependency spring-boot-started-data-jdbc; - in application.properties add new config jdbcTemplate for connection to database; - add new file application.properties.origin indices config for other people;  - in package dao created new class PersonMapper.java, for get dates form database and return them in object person;  - in PersonDAO.java changed all method used jdbcTemplate;

---
+++ spring-mvc/src/main/java/md/miller1995/springmvc/configs/SpringConfig.java
@@ -0,0 +1,46 @@
+//package md.miller1995.springmvc.configs;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.sql.DataSource;
+//
+//
+//@Configuration
+//@ComponentScan("md.miller1995.springmvc")
+//@EnableWebMvc
+//public class SpringConfig implements WebMvcConfigurer {
+//
+//    private final ApplicationContext applicationContext;
+//
+//    @Autowired
+//    public SpringConfig(ApplicationContext applicationContext) {
+//        this.applicationContext = applicationContext;
+//    }
+//
+//    @Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://localhost:5432/springMVC");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("postgres");
+//
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    public JdbcTemplate jdbcTemplate(){
+//        return new JdbcTemplate(dataSource());
+//    }
+//
+//
+//}
