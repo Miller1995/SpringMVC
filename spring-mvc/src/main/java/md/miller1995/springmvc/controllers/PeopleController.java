70ca3cbc6d720725929adc9b8d5069c25bc74304
miller1995 <nircaanton95@gmail.com>
2022-10-26 11:48:30 +0300

    - in pom.xml add new dependency hibernate validator; - in Person.java add new annotation; - in PeopleController.java add annotation @Valid and create a new object BindingResult for get error message; - in editPerson.html and newPerson.html add new tag div for error annotation;

--- spring-mvc/src/main/java/md/miller1995/springmvc/controllers/PeopleController.java
+++ spring-mvc/src/main/java/md/miller1995/springmvc/controllers/PeopleController.java
@@ -5,7 +5,10 @@ import md.miller1995.springmvc.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -41,7 +44,11 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String addPerson(@ModelAttribute("person") Person person){
+    public String addPerson(@ModelAttribute("person") @Valid Person person,
+                            BindingResult bindingResult){
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -55,8 +62,11 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String updatePerson(@ModelAttribute("person") Person person,
+    public String updatePerson(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                                @PathVariable("id") int id){
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
