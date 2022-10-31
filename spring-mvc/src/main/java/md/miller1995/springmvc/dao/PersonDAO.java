9ae122a5b6b94fc9791119fd05e7ea732b85f414
miller1995 <nircaanton95@gmail.com>
2022-10-31 20:25:11 +0200

    - delete PersonMapper.java, PersonValidator.java and BatchController.java; - delete index.html for BatchController.java; - in application.properties add and edit config for hibernate/JPA; - in pom.xml add dependency for data jpa; - in PersonDAO.java delete jdbcTemplate injection and add sessionFactory injection. Edit all method for Hibernate. Remove method for batch test; - delete annotation for validation from Person.java and add annotation for Table, and generated toString;

--- spring-mvc/src/main/java/md/miller1995/springmvc/dao/PersonDAO.java
+++ spring-mvc/src/main/java/md/miller1995/springmvc/dao/PersonDAO.java
@@ -1,100 +1,71 @@
 package md.miller1995.springmvc.dao;
 
 import md.miller1995.springmvc.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+
+
 import java.util.List;
 
 @Component
 public class PersonDAO {
 
-    private final JdbcTemplate jdbcTemplate;
+
+    private final SessionFactory sessionFactory;
 
     @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
-
+    @Transactional(readOnly = true)
     public List<Person> index(){
-        return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper());
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("SELECT p FROM Person p", Person.class).getResultList();
     }
 
+    @Transactional(readOnly = true)
     public Person show(int id){
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?",
-                                                            new Object[]{id}, new PersonMapper()).stream().findAny().orElse(null);
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.get(Person.class, id);
     }
 
-    public Person show(String email){
-        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?",
-                                                            new Object[]{email}, new PersonMapper()).stream().findAny().orElse(null);
-    }
 
+    @Transactional
     public void save(Person person){
-        jdbcTemplate.update("INSERT INTO Person(namePerson, age, email, address) VALUES (?, ?, ?, ?)",
-                                                                                    person.getName(), person.getAge(), person.getEmail(), person.getAddress());
+        Session session = sessionFactory.getCurrentSession();
+
+        session.save(person);
     }
 
+    @Transactional(readOnly = true)    // doesn't work
     public void update(int id, Person updatePerson){
-        jdbcTemplate.update("UPDATE Person SET namePerson=?, age=?, email=?, address=? WHERE id=?",
-                                                    updatePerson.getName(), updatePerson.getAge(), updatePerson.getEmail(), updatePerson.getAddress(), id);
+        Session session = sessionFactory.getCurrentSession();
+
+        Person personToBeUpdated = session.get(Person.class, id);
+
+        personToBeUpdated.setName(updatePerson.getName());
+        personToBeUpdated.setAge(updatePerson.getAge());
+        personToBeUpdated.setEmail(updatePerson.getEmail());
+        personToBeUpdated.setAddress(updatePerson.getAddress());
+
     }
 
+    @Transactional(readOnly = true)    // doesn't work
     public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
-    }
-
-    //--------------------- Test bach update -----------------
-
-    public void testMultipleUpdate(){
-        List<Person> people  = create1000People();
-        long before = System.currentTimeMillis();
-        for (Person person : people) {
-            jdbcTemplate.update("INSERT INTO Person VALUES (?, ?, ?, ?)",
-                                                                person.getId(), person.getName(), person.getAge(), person.getEmail());
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(Person.class, id));
         }
-        long after = System.currentTimeMillis();
-        System.out.println("Time" + (after - before));
-    }
 
-    public void testBatchUpdate(){
-        List<Person> people  = create1000People();
 
-        long before = System.currentTimeMillis();
 
-        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?, ?, ?, ?)", new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-
-                ps.setInt(1, people.get(i).getId());
-                ps.setString(2,people.get(i).getName());
-                ps.setInt(3, people.get(i).getAge());
-                ps.setString(4, people.get(i).getEmail());
-            }
-
-            @Override
-            public int getBatchSize() {
-                return people.size();
-            }
-        });
-
-        long after = System.currentTimeMillis();
-
-        System.out.println("TimeBatch" + (after - before));
-
-    }
-
-    private List<Person> create1000People(){
-        List<Person> people = new ArrayList<>();
-        for (int i = 1; i < 1000; i++){
-            people.add(new Person(i, "Name" + i, 30, "test" + i + "mail.ru", "address"));
-        }
-        return people;
-    }
 }
